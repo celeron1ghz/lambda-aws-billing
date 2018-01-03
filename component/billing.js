31907e8fd9f5aa7ece19d29fc817d9abb83f9550
celeron1ghz <celeron1ghz@gmail.com>
2018-01-03 18:36:17 +0900

    using attachments in slack

--- component/billing.js
+++ component/billing.js
@@ -14,30 +14,36 @@ const s3  = new aws.S3();
 const dimensions = [
     {
         label: 'Total',
+        icon:  'yen',
         param: [{ Name: 'Currency', Value: 'USD' }],
     },{
         label: 'DataTransfer',
+        icon:  'calling',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AWSDataTransfer' }],
         description: billing => new Promise(
             (resolve,reject) => resolve(`(${ (billing['Maximum'] * (100/108) / 0.1376469948).toFixed(2)}GB maybe)`)
         ),
     },{
         label: 'Route53',
+        icon:  'route53',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AmazonRoute53' }],
         description: billing =>
             r53.listHostedZones().promise().then(data => `(${data.HostedZones.length} hosted zone)`),
     },{
         label: 'EC2',
+        icon:  'ec2',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AmazonEC2' }],
         description: billing =>
             ec2.describeInstances().promise().then(data => `(${data.Reservations.length} instances)`),
     },{
         label: 'RDS',
+        icon:  'rds',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AmazonRDS' }],
         description: billing =>
             rds.describeDBInstances().promise().then(data => `(${data.DBInstances.length} instances)`),
     },{
         label: 'S3',
+        icon:  's3',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AmazonS3' }],
         description: billing => {
             const clowdwatch = new aws.CloudWatch({region: 'ap-northeast-1', endpoint: 'http://monitoring.ap-northeast-1.amazonaws.com'});
@@ -73,17 +79,21 @@ const dimensions = [
         },
     },{
         label: 'KMS',
+        icon:  'kms',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'awskms' }],
         description: billing =>
             kms.listAliases().promise().then(data => `(${data.Aliases.filter(v => !v.AliasName.match(/aws/)).length} keys)`),
     },{
         label: 'CodeBuild',
+        icon:  'codebuild',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'CodeBuild' }],
     },{
         label: 'API Gateway',
+        icon:  'apigateway',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AmazonApiGateway' }],
     },{
         label: 'CloudFront',
+        icon:  'cloudfront',
         param: [{ Name: 'Currency', Value: 'USD' }, { Name: 'ServiceName', Value: 'AmazonCloudFront' }],
     }
 ];
@@ -122,15 +132,20 @@ module.exports = config => {
                 console.log(`${dim.label}#description ==> `, opt_result);
             }
 
-            attachments.push(
-                sprintf("*%s* = `$%s` %s", dim.label, billing['Maximum'], opt_result)
-            );
+            attachments.push({
+                title: `:${dim.icon || 'thinking_face'}: ${dim.label}`,
+                value: "*$" + billing['Maximum'] + "* " + opt_result,
+                short: true,
+            });
         }
+
+        const total = attachments.shift();
 
         return {
             username: "AWS Billing",
             icon_emoji: ':money_with_wings:',
-            text: attachments.join('\n'),
+            text: 'Total price is ' + total.value,
+            attachments: [{ mrkdwn_in: ['fields'], fields: attachments }]
         };
     });
 };
